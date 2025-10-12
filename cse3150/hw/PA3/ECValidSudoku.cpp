020eff2f0ce232013abc69e6e3881113e5c6eaaf
gig22002 <gianpaolo.girasoli@uconn.edu>
2025-10-12 18:25:41 -0400

    PA3

--- cse3150/hw/PA3/ECValidSudoku.cpp
+++ cse3150/hw/PA3/ECValidSudoku.cpp
@@ -11,6 +11,8 @@ Note: "." represents a blank sudoku square
 */
 #include <iostream>
 #include <vector>
+#include <string>
+#include <unordered_set>
 
 using namespace std;
 
@@ -18,6 +20,56 @@ using namespace std;
 bool ECValidSudoku(vector<vector<string>> &board){
 	
 	//your code here
+	for(int r=0; r<9; r++){
+		unordered_set<string> map;
+		for(int i=0; i<9; i++){
+			if(board[r][i] == ".") { continue; }
+			if(map.count(board[r][i])!=0){
+				//cout << r << "," << i << endl;
+				//cout << board[r][i] << endl;
+				return false;
+			}
+			map.insert(board[r][i]);
+		}
+	}
+	
+	for(int c=0; c<9; c++){
+		unordered_set<string> map;
+		for(int j=0; j<9; j++){
+			if(board[j][c] == ".") { continue; }
+			if(map.count(board[j][c])!=0)
+				return false;
+			map.insert(board[j][c]);
+		}
+	}
+
+	for(int subI=0; subI<9; subI++){
+		unordered_set<string> map;
+		for(int i=0; i<3; i++){
+			for(int j=0; j<3; j++){
+				int r = (subI/3)*3 + i;
+				int c = (subI%3)*3 + j;
+				if(board[r][c] == ".") { continue; }
+				if(map.count(board[r][c]) != 0)
+					return false;
+				map.insert(board[r][c]);
+			}
+		}
+	}
+	//cout << "true" << endl;
+	
+	/*vector<int> map(10);
+	vector<vector<int>> subMap(10, vector<int>(10));
+	for(vector<string> i : board){
+		map.clear();
+		for(string j : i){
+			if(j == ".") { continue; }
+			if(map[stoi(j)] != 0)
+				return false;
+			map[stoi(j)]++;
+		}	
+	}*/
+
 	return true;
 
 }
