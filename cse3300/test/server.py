5e3eafbad967ec3ed781e011741f764f50d7247d
gio <gianpaolo.girasoli@uconn.edu>
2026-02-27 10:14:13 -0500

    init test

---
+++ cse3300/test/server.py
@@ -0,0 +1,22 @@
+from socket import *
+
+host = "localhost"
+port = 25565
+
+s = socket(AF_INET, SOCK_STREAM)
+s.setsockopt(SOL_SOCKET, SO_KEEPALIVE, 1)
+s.bind((host, port))
+s.listen()
+
+while 1:
+    conn, addr = s.accept()
+    
+    print('Server connected to', addr)
+
+    data = conn.recv(1024)
+    print('Received',data)
+    if not data: 
+        conn.close()
+        break
+    msg = 'ACK'
+    conn.sendall(msg.encode())
