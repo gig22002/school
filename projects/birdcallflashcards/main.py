f3b978b8a4eec09d4526d5bd9856ded5185f2e58
gio <gianpaolo.girasoli@uconn.edu>
2026-03-02 14:58:54 -0500

    support for diff number of options

--- projects/birdcallflashcards/main.py
+++ projects/birdcallflashcards/main.py
@@ -5,14 +5,14 @@ import re
 
 sounds = []
 
-def printOptions(rightAns):
+def printOptions(rightAns, numOptions=4):
+    '''Populate 4 random-ordered options with (at least) one correct answer.'''
     options = [rightAns]
     rightAnsI = []
-    options.append(sounds[random.randint(0,len(sounds)-1)][:-4])
-    options.append(sounds[random.randint(0,len(sounds)-1)][:-4])
-    options.append(sounds[random.randint(0,len(sounds)-1)][:-4])
+    for i in range(0,numOptions-1):
+        options.append(sounds[random.randint(0,len(sounds)-1)][:-4])
 
-    for i in range(0,4):
+    for i in range(0,numOptions):
         x = random.randint(0,len(options)-1)
         print(f"{i+1}: {options[x]}")
         if(options[x] == rightAns): rightAnsI.append(f"{i+1}")
@@ -21,6 +21,7 @@ def printOptions(rightAns):
     return rightAnsI
 
 def getAcceptedAnswers(stringIn):
+    '''Split string in order to output array of accepted parameters: an acronym, split by spaces, and the original file name'''
     ans = re.split(r"[ _+\-]+", stringIn)
     nym = ''
     for x in ans:
@@ -38,14 +39,20 @@ if __name__ == "__main__":
             if (f.name[-4:] == '.mp3'):
                 sounds.append(f.name)
 
+    print("Welcome to Alli's birdcall flashcards!")
+    numOptions = input("Enter the number of options per question: ")
+    try: numOptions = int(numOptions)
+    except: numOptions = 4
     tryAgain = False
     i = random.randint(0,len(sounds)-1)
     while 1:
         if(tryAgain): i = random.randint(0,len(sounds)-1)
         call = playsound(sounds[i], False)
-        corr = printOptions(sounds[i][:-4])
+        #Populate answers and format correct answer
+        corr = printOptions(sounds[i][:-4], numOptions)
         corrAns = getAcceptedAnswers(sounds[i][:-4].lower())
         ans = input("Input your answer: ")
+        #Do not select a different birdcall if incorrect
         if(ans.lower() in corrAns or ans in corr):
             print("Correct!")
             tryAgain = False
