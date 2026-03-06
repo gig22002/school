b6d3b3a12297ce57eb093dc99e2b81753dd76587
gio <gianpaolo.girasoli@uconn.edu>
2026-03-06 11:32:17 -0500

    reorganize files

---
+++ projects/encryptedmessages/mk1/secserv.py
@@ -0,0 +1,33 @@
+from socket import *
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
+
+#cryptography
+path = input("Input private key path: ")
+pwd = input("Input password: ")
+debug = input("Debug mode? 0/1: ")
+sk = RSA.importKey(open('sk.pem').read(), pwd)
+cipher = PKCS1_OAEP.new(sk)
+
+host = "localhost"
+port = 25565
+
+s = socket(AF_INET, SOCK_STREAM)
+s.setsockopt(SOL_SOCKET, SO_KEEPALIVE, 1)
+s.bind((host, port))
+s.listen()
+
+while 1:
+    conn, addr = s.accept()
+    
+    print('Server connected to', addr)
+
+    data = conn.recv(1024)
+    if(debug): print(data.hex())
+    dec = cipher.decrypt(data)
+    print('Received',dec)
+    if not data: 
+        break
+    msg = 'ACK'
+    conn.sendall(msg.encode())
+    conn.close()
