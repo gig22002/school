0574797f674593261aa4a58a8802e00ec979dda1
gio <gianpaolo.girasoli@uconn.edu>
2026-03-11 14:42:15 -0400

    purchase ticket

--- cse3550/hw2/raffle/src/Raffle.sol
+++ cse3550/hw2/raffle/src/Raffle.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.13;
 contract Raffle {
 	address payable owner;
 	event failedInit(address addr);
-	mapping(address => int32) registered;
+	mapping(address => uint8) registered;
 	event failedReg(address addr);
 	event deregistered(address addr);
 	uint256 potAmt;
 	uint256 regFee = type(uint256).max;
 	uint256 ticketPrice;
-	uint256 minPlayers;
+	event boughtTicket(address addr, uint8 n);
+	uint32 curPlayers = 0;
+	uint32 minPlayers;
 
 	constructor() public {
 		owner = payable(msg.sender);
@@ -29,7 +31,7 @@ contract Raffle {
 	}
 
 	function register() public payable {
-		if (msg.value<regFee || msg.sender == owner) {
+		if (msg.value<regFee || msg.sender == owner || registered[msg.sender] == 0 ) {
 			emit failedReg(msg.sender);
 			return;
 		}
@@ -43,11 +45,23 @@ contract Raffle {
 	}
 
 	function deregister() public {
-		if (registered[msg.sender] == 0)
+		if (registered[msg.sender] == 0 || registered[msg.sender] > 1)
 			return;
 		registered[msg.sender] = 0;
 		payable(msg.sender).transfer(regFee);
 		emit deregistered(msg.sender);
+	}
+
+	function purchaseTicket(uint8 _amt) public payable {
+		if (registered[msg.sender] == 0 || registered[msg.sender]+_amt > 21) 
+			return;
+		if (msg.value < _amt*ticketPrice)
+			return;
+		potAmt += _amt*ticketPrice;
+		if (msg.value > _amt*ticketPrice)
+			payable(msg.sender).transfer(msg.value-_amt*ticketPrice);
+		emit boughtTicket(msg.sender, _amt);
+
 	}
 	
 }
