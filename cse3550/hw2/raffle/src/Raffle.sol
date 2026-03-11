c6961c9d05addba2b7ca1298982dfd98ed35fb02
gio <gianpaolo.girasoli@uconn.edu>
2026-03-11 12:52:37 -0400

    init raffle

--- cse3550/hw2/raffle/src/Raffle.sol
+++ cse3550/hw2/raffle/src/Raffle.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 contract Raffle {
 	address payable owner;
+	event failedInit(address addr);
 	mapping(address => uint256) registered;
 	event failedReg(address addr);
 	uint256 potAmt;
@@ -13,12 +14,32 @@ contract Raffle {
 	constructor() public {
 		owner = payable(msg.sender);
 	}
-
-	function register(address registree, uint256 amt) public {
-		if (amt<regFee) {
-			emit failedReg(registree);
+	
+	function setUpRaffle(uint256 _pot, uint256 _regFee, uint256 _price, uint256 _min) public {
+		if(msg.sender != owner) {
+			emit failedInit(msg.sender);
 			return;
 		}
+
+		potAmt = _pot;
+		regFee = _regFee;
+		ticketPrice = _price;
+		minPlayers = _min;
+	}
+
+	function register() public payable {
+		if (msg.value<regFee || msg.sender == owner) {
+			emit failedReg(msg.sender);
+			return;
+		}
+		//Initialize registree
+		registered[msg.sender] = 0;
+		owner.transfer(regFee);
+
+		if (msg.value>regFee) {
+			payable(msg.sender).transfer(msg.value-regFee);
+		}
+
 	}
 	
 }
