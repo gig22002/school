afac729aebedc5549035eec25e5ec28e66a900b8
gio <gianpaolo.girasoli@uconn.edu>
2026-03-11 13:10:11 -0400

    deregister

--- cse3550/hw2/raffle/src/Raffle.sol
+++ cse3550/hw2/raffle/src/Raffle.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.13;
 contract Raffle {
 	address payable owner;
 	event failedInit(address addr);
-	mapping(address => uint256) registered;
+	mapping(address => int32) registered;
 	event failedReg(address addr);
+	event deregistered(address addr);
 	uint256 potAmt;
-	uint256 regFee;
+	uint256 regFee = type(uint256).max;
 	uint256 ticketPrice;
 	uint256 minPlayers;
 
@@ -15,13 +16,13 @@ contract Raffle {
 		owner = payable(msg.sender);
 	}
 	
-	function setUpRaffle(uint256 _pot, uint256 _regFee, uint256 _price, uint256 _min) public {
+	function setUpRaffle(uint256 _regFee, uint256 _price, uint256 _min) public payable {
 		if(msg.sender != owner) {
 			emit failedInit(msg.sender);
 			return;
 		}
 
-		potAmt = _pot;
+		potAmt = msg.value;
 		regFee = _regFee;
 		ticketPrice = _price;
 		minPlayers = _min;
@@ -33,13 +34,20 @@ contract Raffle {
 			return;
 		}
 		//Initialize registree
-		registered[msg.sender] = 0;
-		owner.transfer(regFee);
+		registered[msg.sender] = 1;
 
 		if (msg.value>regFee) {
 			payable(msg.sender).transfer(msg.value-regFee);
 		}
 
+	}
+
+	function deregister() public {
+		if (registered[msg.sender] == 0)
+			return;
+		registered[msg.sender] = 0;
+		payable(msg.sender).transfer(regFee);
+		emit deregistered(msg.sender);
 	}
 	
 }
